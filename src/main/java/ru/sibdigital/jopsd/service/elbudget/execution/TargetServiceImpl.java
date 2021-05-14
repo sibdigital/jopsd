ddea4bae1c217558630d31738eb7bef6e73e9ed3
liudmilagy <gonchikova94@gmail.com>
2021-05-14 10:06:22 +0800

    targets

--- src/main/java/ru/sibdigital/jopsd/service/elbudget/execution/TargetServiceImpl.java
+++ src/main/java/ru/sibdigital/jopsd/service/elbudget/execution/TargetServiceImpl.java
@@ -1,17 +1,24 @@
 package ru.sibdigital.jopsd.service.elbudget.execution;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import ru.sibdigital.jopsd.dto.elbudget.execution.Resultsexecution;
+import ru.sibdigital.jopsd.model.WorkPackage;
+import ru.sibdigital.jopsd.model.WorkPackageTarget;
 import ru.sibdigital.jopsd.service.SuperServiceImpl;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 
+@Service
+@Slf4j
 public class TargetServiceImpl extends SuperServiceImpl implements TargetService {
     @Override
     public void saveTargets(Resultsexecution.RegProject regProject, Map<String, Object> params) {
         List<Resultsexecution.RegProject.PurposeCriterias.PurposeCriteria> criteriaList = getPurposeCriteriaList(regProject);
         for (Resultsexecution.RegProject.PurposeCriterias.PurposeCriteria criteria : criteriaList) {
-
+            List<WorkPackageTarget> targets = parsePurposeCriteria(criteria, params);
         }
     }
 
@@ -23,5 +30,37 @@ public class TargetServiceImpl extends SuperServiceImpl implements TargetService
         }
 
         return purposeCriteriaList;
+    }
+
+    private List<WorkPackageTarget> parsePurposeCriteria(Resultsexecution.RegProject.PurposeCriterias.PurposeCriteria criteria, Map<String, Object> params) {
+        List<WorkPackageTarget> targets = null;
+        Long workPackageId = (Long) params.get("workPackageId");
+        WorkPackage workPackage = workPackageRepo.findById(workPackageId).orElse(null);
+        Long projectId = (workPackage == null) ? null : workPackage.getProjectId();
+
+        Resultsexecution.RegProject.PurposeCriterias.PurposeCriteria.PurposeCriteriaMonthlyExecutions monthlyExecutions = criteria.getPurposeCriteriaMonthlyExecutions();
+        if (monthlyExecutions != null) {
+            List<Resultsexecution.RegProject.PurposeCriterias.PurposeCriteria.PurposeCriteriaMonthlyExecutions.PurposeCriteriaMonthlyExecution> monthlyExecutionList =
+                monthlyExecutions.getPurposeCriteriaMonthlyExecution();
+
+            for (Resultsexecution.RegProject.PurposeCriterias.PurposeCriteria.PurposeCriteriaMonthlyExecutions.PurposeCriteriaMonthlyExecution monthlyExecution : monthlyExecutionList) {
+                WorkPackageTarget workPackageTarget= WorkPackageTarget.builder()
+                        .projectId(projectId)
+                        .workPackageId(workPackageId)
+//                        .targetId(null)
+//                        .year()
+//                        .quarter()
+                        .month(Long.parseLong(monthlyExecution.getMonth()))
+//                        .value()
+//                        .type()
+                        .createdAt(new Timestamp(System.currentTimeMillis()))
+                        .updatedAt(new Timestamp(System.currentTimeMillis()))
+//                        .planValue()
+                      .build();
+                targets.add(workPackageTarget);
+            }
+        }
+
+        return targets;
     }
 }
