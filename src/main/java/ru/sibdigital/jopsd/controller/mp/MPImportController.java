3d47a4b3210b5df39b3781bfe66418327e99b9bb
liudmila <gonchikova94@gmail.com>
2021-09-08 17:01:00 +0800

    backend for angular mpp uploader

--- src/main/java/ru/sibdigital/jopsd/controller/mp/MPImportController.java
+++ src/main/java/ru/sibdigital/jopsd/controller/mp/MPImportController.java
@@ -2,11 +2,14 @@ package ru.sibdigital.jopsd.controller.mp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
@@ -28,44 +31,28 @@ public class MPImportController extends SuperController {
     @Autowired
     ProjectService projectService;
 
-    @GetMapping("/upload_mpp")
-    public String viewAppointment(Model model, HttpSession session) {
-       List<Project> projectList = projectService.getProjects();
-        model.addAttribute("projectList", projectList);
-        return "upload_mpp";
-    }
-
     @PostMapping("/import/mpp")
-    public RedirectView importMpp(@RequestParam("file") MultipartFile multipartFile,
-                                            @RequestParam("projectId") Long projectId,
-//                                            @RequestParam("authorId") Long authorId,
-                                            RedirectAttributes redirectAttributes
+    public @ResponseBody Object importMpp(@RequestParam("file") MultipartFile multipartFile,
+                             @RequestParam("projectId") Long projectId,
+                             @RequestParam("authorId") Long authorId
     ) {
         try {
             InputStream inputStream = multipartFile.getInputStream();
 
             Map<String, Object> params = new HashMap<>();
             params.put("projectId", projectId);
-//            params.put("authorId", authorId);
-            params.put("authorId", Long.valueOf(2));
+            params.put("authorId", authorId);
 
             List<WorkPackage> workPackages = MPService.importFile(inputStream, params);
-            redirectAttributes.addAttribute("work_packages", workPackages);
-
-            return new RedirectView("uploaded_work_packages");
+            return workPackages;
 
         } catch (Exception e) {
             logError(e);
-            return null;
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("{\"status\": \"server\"," +
+                            "\"cause\":\"Ошибка сохранения\"," +
+                            "\"sname\": \" project-id:" + projectId + "\"}");
         }
     }
 
-    @GetMapping(value = "/import/uploaded_work_packages")
-    public String showUploadedWorkPackages(@RequestParam("work_packages") List<WorkPackage> workPackages,
-                                           Model model) {
-        model.addAttribute("work_packages", workPackages);
-        model.addAttribute("ref_portal", applicationConstants.getRefTestPortal());
-//        model.addAttribute("ref_portal", applicationConstants.getRefWorkingPortal);
-        return "uploaded_work_packages";
-    }
 }
