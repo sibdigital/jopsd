bb2930a5078c3040469755dc6b7a2951ae3344af
liudmila <gonchikova94@gmail.com>
2021-09-10 17:08:44 +0800

    close access for some table's data for rest api

--- src/main/java/ru/sibdigital/jopsd/repository/opsd/SessionRepository.java
+++ src/main/java/ru/sibdigital/jopsd/repository/opsd/SessionRepository.java
@@ -2,9 +2,9 @@ package ru.sibdigital.jopsd.repository.opsd;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.sibdigital.jopsd.model.opsd.Session;
 
-@Repository
+@RepositoryRestResource(exported = false)
 public interface SessionRepository extends JpaRepository<Session, Long>, JpaSpecificationExecutor<Session> {
 }
