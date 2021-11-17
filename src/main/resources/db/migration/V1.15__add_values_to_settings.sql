ef0508d54a8fd589be1924135a9dcda52706f0cb
Mqter <monstor-beats2015@yandex.ru>
2021-11-17 10:15:24 +0800

    create migration for broker_bot

---
+++ src/main/resources/db/migration/V1.15__add_values_to_settings.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.settings (name, value) VALUES ('event_via_14_day', 'WP_VIA_14_DAY');
+INSERT INTO public.settings (name, value) VALUES ('event_proj_reestr', 'PROJ_REESTR');
+INSERT INTO public.settings (name, value) VALUES ('event_proj_reestr_elem', 'PROJ_REESTR_ELEM');
+INSERT INTO public.settings (name, value) VALUES ('url_request_brbo', '/request');
+INSERT INTO public.settings (name, value) VALUES ('url_event_parent_brbo', '/event_type/parentEvents');
+INSERT INTO public.settings (name, value) VALUES ('target_system_code', 'ISUP');
+INSERT INTO public.settings (name, value) VALUES ('url_message_brbo', '/message');
+INSERT INTO public.settings (name, value) VALUES ('protocol_brbo', 'http');
+INSERT INTO public.settings (name, value) VALUES ('url_brbo', '188.72.76.241');
+INSERT INTO public.settings (name, value) VALUES ('url_port_brbo', '8075');
+INSERT INTO public.settings (name, value) VALUES ('url_context_path_brbo', 'api');
+INSERT INTO public.settings (name, value) VALUES ('event_wp_status', 'WP_STATUS');
+INSERT INTO public.settings (name, value) VALUES ('event_wp_overdue', 'WP_OVERDUE');
+INSERT INTO public.settings (name, value) VALUES ('event_proj_members', 'PROJ_MEMBERS');
+INSERT INTO public.settings (name, value) VALUES ('event_proj_members_elem', 'PROJ_MEMBERS_ELEM');
+INSERT INTO public.settings (name, value) VALUES ('event_meetings_overdue_memb', 'MEETINGS_OVERDUE_MEMB');
