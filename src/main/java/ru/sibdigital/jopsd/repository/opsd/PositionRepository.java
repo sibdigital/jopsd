475411688537daf4981f4e92132916f4bf493571
Victor <victor98@bk.ru>
2022-01-13 11:34:39 +0800

    feature-1370 Добавлена сортировка справочника должностей

--- src/main/java/ru/sibdigital/jopsd/repository/opsd/PositionRepository.java
+++ src/main/java/ru/sibdigital/jopsd/repository/opsd/PositionRepository.java
@@ -1,9 +1,12 @@
 package ru.sibdigital.jopsd.repository.opsd;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import ru.sibdigital.jopsd.model.opsd.Organization;
 import ru.sibdigital.jopsd.model.opsd.Position;
 
 @RepositoryRestResource
@@ -16,4 +19,6 @@ public interface PositionRepository extends JpaRepository<Position, Long>, JpaSp
     @Override
     @RestResource(exported = false)
     void delete(Position position);
+
+    Page<Position> findAllByIdIsNotNullOrderByNameAsc(Pageable pageable);
 }
